6b3c020e19be935b96c2808ae71b76d66a262a8b
XuanManh9999 <nguyenxuanmanh030929@gmail.com>
2024-06-12 23:19:46 +0700

    Add UI

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('home/', views.home, name="home"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
+    path('', views.login, name="login"),
+    path('register/', views.register, name="register"),
+    path('forgot_password/', views.forgot_password, name="forgot_password"),
 ]
--- app/views.py
+++ app/views.py
@@ -3,4 +3,24 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'app/home.html')
+    context = {}
+    return render(request, 'app/home.html', context)
+
+def cart(request):
+    context = {}
+    return render(request, 'app/cart.html', context)
+
+def checkout(request):
+    context = {}
+    return render(request, 'app/checkout.html', context)
+
+def login(request):
+    context = {}
+    return render(request, 'auth/login.html', context)
+def register(request):
+    context = {}
+    return render(request, 'auth/register.html', context)
+
+def forgot_password(request):
+    context = {}
+    return render(request, 'auth/forgot_password.html', context)
